0663feaccebb6f801cb755ede233af0c7e9f336d
Евгения Загородных <evgeniazagorodnykh@mail.ru>
2023-12-09 17:54:56 +0300

    add tests

--- src/test/java/OfficeList/MyCsvReaderTest.java
+++ src/test/java/OfficeList/MyCsvReaderTest.java
@@ -2,12 +2,19 @@ package OfficeList;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyCsvReaderTest {
 
     @Test
-    void testRead() {
+    void readRightTest() throws Exception {
+        MyCsvReader reader = new MyCsvReader();
+        List<Worker> testOffice = reader.read(new File("src/main/resources/test1.csv"));
 
+        assertEquals("20000 Evgenia Female I 1000.0 04.08.2003",
+                testOffice.get(0).toString());
     }
 }
